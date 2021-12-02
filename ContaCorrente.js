9047a74df8317756007484d2480504f15a54601e
Hugo Cesar <hugocesarcaldeira@gmail.com>
2021-12-02 14:20:44 -0300

    Corrigindo erro de import e log do saldo

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,9 +1,22 @@
-export class ContaCorrente{
+export class ContaCorrente {
     agencia;
 
     /* Atributo Privado */
     #saldo;
 
-    depositar(valor) { if(valor > 0) this.#saldo += valor; }
-    sacar(valor) { if(this.#saldo >= valor) this.#saldo -= valor; }
+    depositar(valor) {
+
+        if(valor > 0)
+            this.#saldo += valor;
+
+        console.log(`Valor após deposito: ${this.#saldo}`);
+    }
+
+    sacar(valor) {
+
+        if(this.#saldo >= valor)
+            this.#saldo -= valor;
+
+        console.log(`Valor após saque: ${this.#saldo}`);
+    }
 }
