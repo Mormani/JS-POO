ad9c09c7e56d540173130152593e04e23cae1042
Hugo Cesar <hugocesarcaldeira@gmail.com>
2021-12-03 13:51:18 -0300

    Adicionando metodo de transferencia para Conta Corrente

--- Primeiro-Curso/ContaCorrente.js
+++ Primeiro-Curso/ContaCorrente.js
@@ -14,14 +14,30 @@ export class ContaCorrente {
 
     get saldo() { return this.#saldo }
 
-    constructor(agencia, cliente) { this.agencia = agencia; this.cliente = cliente; ContaCorrente.numDeContas++; };
+    constructor(agencia, cliente) {
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numDeContas++;
+    }
 
-    depositar(valor) { if(valor > 0) this.#saldo += valor; }
-    sacar(valor) { if(this.#saldo >= valor) this.#saldo -= valor; }
+    depositar(valor) {
+
+        if(valor <= 0) { return; }
+
+        return this.#saldo += valor;
+    }
+
+    sacar(valor) {
+        if(this.#saldo >= valor) {
+
+            this.#saldo -= valor;
+            return valor;
+        }
+    }
 
     transferir(valor, conta) {
 
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado)
+        let valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
     }
 }
--- Primeiro-Curso/index.js
+++ Primeiro-Curso/index.js
@@ -7,9 +7,7 @@ const ccRicardo = new ContaCorrente(1001, cliente1);
 console.log(ccRicardo);
 console.log(ccRicardo.cliente);
 
-console.log(ccRicardo);
-
-ccRicardo.depositar(100);
+ccRicardo.depositar(500);
 console.log(`Valor após depósito: ${ccRicardo.saldo}`);
 
 ccRicardo.sacar(50);
@@ -19,3 +17,8 @@ const cliente2 = new Cliente("Alice", 88822233309);
 const ccAlice = new ContaCorrente(1002, cliente2);
 
 console.log(`Número de contas correntes: ${ContaCorrente.numDeContas}`);
+
+ccRicardo.transferir(20, ccAlice)
+
+console.log(ccRicardo);
+console.log(ccAlice);
