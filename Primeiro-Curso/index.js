5f22cedd5e5bfff4ba2af5d946e9ea5191eecd42
Hugo Cesar <hugocesarcaldeira@gmail.com>
2021-12-03 13:58:24 -0300

    Adicionando e formatando informacoes no console

--- Primeiro-Curso/index.js
+++ Primeiro-Curso/index.js
@@ -8,17 +8,19 @@ console.log(ccRicardo);
 console.log(ccRicardo.cliente);
 
 ccRicardo.depositar(500);
-console.log(`Valor após depósito: ${ccRicardo.saldo}`);
+console.log(`\nValor após depósito para Ricardo: ${ccRicardo.saldo}`);
 
 ccRicardo.sacar(50);
-console.log(`Valor após saque: ${ccRicardo.saldo}`);
+console.log(`Valor após saque do Ricardo: ${ccRicardo.saldo}\n`);
 
 const cliente2 = new Cliente("Alice", 88822233309);
 const ccAlice = new ContaCorrente(1002, cliente2);
+console.log(ccAlice);
+console.log(ccAlice.cliente);
 
-console.log(`Número de contas correntes: ${ContaCorrente.numDeContas}`);
+console.log(`\nNúmero de contas correntes: ${ContaCorrente.numDeContas}`);
 
 ccRicardo.transferir(20, ccAlice)
 
-console.log(ccRicardo);
-console.log(ccAlice);
+console.log(`\nSaldo após transferência de Ricardo: ${ccRicardo.saldo}`);
+console.log(`Saldo após transferência para Alice: ${ccAlice.saldo}`);
