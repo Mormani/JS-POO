d0b6586cfa249a18c88ca8553ea4a1bff615e54c
Hugo Cesar <hugocesarcaldeira@gmail.com>
2021-12-06 02:22:14 -0300

    Verificando se existe metodo para autenticar senha

--- Segundo-Curso/SistemaAutenticacao.js
+++ Segundo-Curso/SistemaAutenticacao.js
@@ -3,6 +3,15 @@ export class SistemaAutenticação {
     // Metódos Públicos
     static login(auth, senha) {
 
-        return auth.autenticar(senha);
+        if(SistemaAutenticação.ehAuth(auth))
+            return auth.autenticar(senha);
+
+        return false;
+    }
+
+    static ehAuth(auth) {
+
+        return "autenticar" in auth &&
+                auth.autenticar instanceof Function;
     }
 }
